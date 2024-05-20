0c88792c18427271ad4767bd1d3b259d9974646e
alexeyeryshev <a.a.eryshev@gmail.com>
2024-05-20 18:48:26 +0100

    adjust configs for new laptop hardware

--- fish/config.fish
+++ fish/config.fish
@@ -112,8 +112,17 @@ contains $HOME/dev/personal/flutter/flutter/bin $PATH; or set PATH $HOME/dev/per
 # latest Ruby version and gems
 fish_add_path /opt/homebrew/opt/ruby/bin
 fish_add_path /opt/homebrew/lib/ruby/gems/3.3.0/bin
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
+end
 # <<< conda initialize <<<
 
