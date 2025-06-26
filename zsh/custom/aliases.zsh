2910ae1ac36947499995f252de281c2b12a7c16f
alexeyeryshev <a.a.eryshev@gmail.com>
2025-06-26 22:09:05 +0200

    add up alias

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,2 +1,5 @@
 alias zs="source ~/.zshrc"
 alias gt='git for-each-ref --sort=creatordate --format '\''%(refname) %(creatordate)'\'' refs/tags | sed '\''s/refs\/tags\///'\'' | tail'
+alias up="cd .."
+alias up2="cd ../.."
+alias up3="cd ../../.."
