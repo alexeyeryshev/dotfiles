ef1e654cc745c64c12ba3928939c9ebf8bd83f44
alexeyeryshev <a.a.eryshev@gmail.com>
2025-07-07 20:31:54 +0200

    remove some os x defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -21,29 +21,7 @@ chflags nohidden ~/Library
 
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
-
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-# defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-# defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-
-# Run the screensaver if we're in the bottom-left hot corner.
-# defaults write com.apple.dock wvous-bl-corner -int 5
-# defaults write com.apple.dock wvous-bl-modifier -int 0
-
-# Hide Safari's bookmark bar.
-# defaults write com.apple.Safari ShowFavoritesBar -bool false
-
-# Set up Safari for development.
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-defaults write com.apple.Safari IncludeDevelopMenu -bool true
-defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
-defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
-defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
-
-# Set up ITerm2 configs
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iterm2"
-defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
-defaults write com.apple.universalaccess mouseDriverCursorSize 3.0
+defaults write NSGlobalDomain InitialKeyRepeat -int 1
 
 # Disable natural-scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool FALSE
