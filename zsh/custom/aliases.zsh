32a49d5e03fc13639395b6d22ceed0251689ac19
alexeyeryshev <a.a.eryshev@gmail.com>
2026-01-04 21:45:31 +0100

    add new aliases to zsh

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,12 +1,13 @@
 alias zs="source ~/.zshrc"
 
-alias gt='git for-each-ref --sort=creatordate --format '\''%(refname) %(creatordate)'\'' refs/tags | sed '\''s/refs\/tags\///'\'' | tail'
-alias gcm='git commit -m'
-
 alias up="cd .."
 alias up2="cd ../.."
 alias up3="cd ../../.."
 
 alias kc="kubectx"
 
+# Git
 alias ghw='gh run watch $(gh run list --limit 1 --json databaseId -u alexeyeryshev --jq ".[0].databaseId")'
+alias ghpw='gh pr view'
+alias gt='git for-each-ref --sort=creatordate --format '\''%(refname) %(creatordate)'\'' refs/tags | sed '\''s/refs\/tags\///'\'' | tail'
+alias gcm='git commit -m'
